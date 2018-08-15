278a03cd4f095c39f0b1ba45c854afb3e5dd158a
Alexander <alexander.botello@g.austincc.edu>
2018-08-15 17:56:58 -0500

    Extend Downloader to have converter functionality

--- downloader.py
+++ downloader.py
@@ -1,21 +1,32 @@
 import os
 from pytube import YouTube
+from pydub import AudioSegment
 
-from converter import Converter
 
 class Downloader:
     def __init__(self, url):
         self.yt = YouTube(url)
         self.yt.register_on_complete_callback(self.convert_to_mp3)
         self.stream = self.yt.streams.first()
-        self.converter = Converter(self.filename)
+        self.logger = self.configure_logging()
+
+    def configure_logging(self):
+        pass
 
     def download(self):
         self.stream.download()
 
     def convert_to_mp3(self, stream, file_handle):
-        self.converter.export()
+        self.export(self.filename)
         self.remove()
+
+    def export(self, file_handle):
+        file = self.filename.split('.')[0] + ".mp3"
+        try:
+            audio = AudioSegment.from_file(file_handle)
+            audio.export(file, format="mp3", bitrate="128k")
+        except KeyError:
+            raise ConvertError
 
     def remove(self):
         os.remove(self.filename)
@@ -23,3 +34,10 @@ class Downloader:
     @property
     def filename(self):
         return self.stream.default_filename
+
+
+class ConvertError(Exception):
+    """
+    Throws when file fails to convert to mp3
+    """
+    pass
