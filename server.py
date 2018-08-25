a914439910983600b08cdd69be5e5af4eca0818e
Alexander <alexander.botello@g.austincc.edu>
2018-08-25 16:57:55 -0500

    Refactor download route to actually work

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 import json
 import functools
 
-from flask import Flask, send_file, jsonify, request
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 
 from downloader import Downloader, ConvertError
@@ -12,54 +12,65 @@ from utils import download_by_query
 app = Flask(__name__)
 CORS(app)
 
-def authenticate():
-    message = {'error': "Authentication is required."}
-    resp = jsonify(message)
+# def authenticate():
+#     message = {'error': "Authentication is required."}
+#     resp = jsonify(message)
 
-    resp.status_code = 401
-    resp.headers['WWW-Authenticate'] = 'Basic realm="Main"'
-    return resp
+#     resp.status_code = 401
+#     resp.headers['WWW-Authenticate'] = 'Basic realm="Main"'
+#     return resp
 
-def requires_authorization(f):
-    @functools.wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.headers.get('Authorization')
-        token = os.environ['AUTH_TOKEN']
-        if not auth or auth != token:
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
+# def requires_authorization(f):
+#     @functools.wraps(f)
+#     def decorated(*args, **kwargs):
+#         auth = request.headers.get('Authorization')
+#         token = os.environ['AUTH_TOKEN']
+#         if not auth or auth != token:
+#             return authenticate()
+#         return f(*args, **kwargs)
+#     return decorated
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'msg': "api is running"})
 
 @app.route('/download', methods=['GET'])
-@requires_authorization
-def download():
+# @requires_authorization
+def stream_mp3():
     url = request.args.get('url')
+    file = download_from_youtube(url)
+
+    def generate():
+        with open(file, "rb") as mp3:
+            data = mp3.read(1024)
+            while data:
+                yield data
+                data = mp3.read(1024)
+        delete_audio_file(file)
+
+    return Response(
+        generate(),
+        mimetype="audio/mpeg",
+        headers={"Content-Disposition":
+                 f"attachment; filename={file}",
+                 "Content-Type": "application/octet-stream"})
+
+
+def download_from_youtube(url):
     audio = Downloader(url)
     try:
         audio.download()
-        response = send_file(
-            audio.file,
-            mimetype="audio/mpeg",
-            as_attachment=True,
-            attachment_filename=audio.file
-        )
     except ConvertError:
         audio = download_by_query(audio.title)
-        response = send_file(
-            audio.file,
-            mimetype="audio/mpeg",
-            as_attachment=True,
-            attachment_filename=audio.file
-        )
-    finally:
-        response.headers['Content-Type'] = 'audio/mpeg'
-        os.remove(audio.file)
-        return response
+    return audio.file
 
+
+def delete_audio_file(file):
+    try:
+        os.remove(file)
+    except FileNotFoundError:
+        print('ERRORING INSIDE GENERATE')
+        return
 
 
 if __name__ == "__main__":
